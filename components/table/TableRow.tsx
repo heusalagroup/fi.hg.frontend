b9412b7f6c52fa0b05968e159068565e8aafe68c
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-15 22:57:55 +0300

    Improved table styles

--- components/table/TableRow.tsx
+++ components/table/TableRow.tsx
@@ -1,21 +1,27 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { TABLE_ROW_CLASS_NAME } from "../../constants/hgClassName";
-import "./TableRow.scss";
 import { ReactNode } from "react";
+import "./TableRow.scss";
 
 export interface TableRowProps {
     readonly className ?: string;
     readonly children ?: ReactNode;
+    readonly first ?: boolean;
+    readonly last ?: boolean;
 }
 
 export function TableRow (props: TableRowProps) {
     const className = props?.className;
     const children = props?.children;
+    const first = props?.first;
+    const last = props?.last;
     return (
         <tr className={
             TABLE_ROW_CLASS_NAME
             + (className? ` ${className}` : '')
+            + (first ? ' ' + TABLE_ROW_CLASS_NAME + '-first' : '')
+            + (last ? ' ' + TABLE_ROW_CLASS_NAME + '-last' : '')
         }>{children}</tr>
     );
 }
