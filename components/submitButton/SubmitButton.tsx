da5fac6b9fbcb26bfdd5b8dd724354153c99f949
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-19 16:53:09 +0300

    Added style option to SubmitButton and fixed styles

--- components/submitButton/SubmitButton.tsx
+++ components/submitButton/SubmitButton.tsx
@@ -4,21 +4,26 @@ import { ReactNode } from "react";
 import { SUBMIT_BUTTON_CLASS_NAME } from "../../constants/hgClassName";
 import { Button } from "../button/Button";
 import { ButtonType } from "../button/types/ButtonType";
+import { ButtonStyle } from "../button/types/ButtonStyle";
 
 export interface SubmitButtonProps {
     readonly className ?: string;
     readonly children  ?: ReactNode;
+    readonly style  ?: ButtonStyle;
 }
 
 export function SubmitButton (props: SubmitButtonProps) {
     const className = props?.className;
+    const style = props?.style ?? ButtonStyle.PRIMARY;
+    const children = props?.children;
     return (
         <Button
             type={ButtonType.SUBMIT}
+            style={style}
             className={
                 SUBMIT_BUTTON_CLASS_NAME
                 + (className? ` ${className}` : '')
             }
-        >{props.children}</Button>
+        >{children}</Button>
     );
 }
