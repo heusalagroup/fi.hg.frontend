a739360c263b400dde3286a34695e20756755286
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-22 20:23:41 +0300

    Small syntax fix

--- hooks/useStoreProductList.ts
+++ hooks/useStoreProductList.ts
@@ -6,25 +6,18 @@ import { Product } from "../../core/store/types/product/Product";
 import { ProductType } from "../../core/store/types/product/ProductType";
 
 export function useStoreProductList (type ?: ProductType) : Product[] {
-
     const [ sellerMap, setSendanorProductList ] = useState<Product[]>(type ? StoreProductService.getProductsByType(type) : StoreProductService.getAllProducts() );
-
     // When session service changes data
     useEffect(() => {
-
         if (!StoreProductService.isInitialized() && !StoreProductService.isLoading() ) {
             StoreProductService.refreshProducts();
         }
-
         return StoreProductService.on(
             StoreProductService.Event.UPDATED,
             () => {
                 setSendanorProductList( type ? StoreProductService.getProductsByType(type) : StoreProductService.getAllProducts() );
             }
         );
-
     });
-
     return sellerMap;
-
 }
