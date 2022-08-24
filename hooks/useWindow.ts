278d283c38c38b07b04d5929103a1ac147dd969a
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-24 19:08:43 +0300

    Improved useWindow

--- hooks/useWindow.ts
+++ hooks/useWindow.ts
@@ -1,7 +1,23 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { WindowObjectService } from "../../core/WindowObjectService";
+import { useEffect, useState } from "react";
 
+/**
+ * SSR safe use of window object
+ */
 export function useWindow () : Window | undefined {
-    return WindowObjectService.getWindow();
+    const [w, setW] = useState<Window | undefined>(undefined);
+    useEffect(
+        () => {
+            const ww = WindowObjectService.getWindow();
+            if (!ww) {
+                setW(ww);
+            }
+        },
+        [
+            setW
+        ]
+    );
+    return w;
 }
