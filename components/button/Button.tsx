cc9f66053d29d5ee6829961a8e11bc64426e80f3
eXtranium <83911635+eXtranium@users.noreply.github.com>
2023-03-09 17:16:49 +0200

    Copyright years fixed.

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+// Copyright (c) 2022-2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
 import {
