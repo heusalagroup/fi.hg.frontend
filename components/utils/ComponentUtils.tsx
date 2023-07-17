0e12d9eb63d8f04898d8179f351647f6d12a6a6a
Jaakko Heusala <jhh@hg.fi>
2023-07-17 21:06:45 +0300

    Added functions to get string presentation

--- components/utils/ComponentUtils.tsx
+++ components/utils/ComponentUtils.tsx
@@ -73,6 +73,37 @@ export class ComponentUtils {
         );
     }
 
+    public static prepareParagraphNodesAsText (
+        t: TranslationFunction,
+        description: readonly any[] | undefined,
+        paramBlocks: ComponentNodeMap | undefined,
+        translationParams ?: TranslationParams
+    ) : string {
+        return (
+            description
+            ? map(description, (paragraph: string | ReactNode, i: number) : string => {
+
+                if ( !isString(paragraph) ) {
+                    if (paragraph?.toString) return paragraph.toString();
+                    return '';
+                }
+
+                if ( paramBlocks ) {
+                    return ComponentUtils.prepareNodeStringsFromString(
+                        t(paragraph, translationParams),
+                        paramBlocks,
+                        t,
+                        translationParams
+                    ).join(' ');
+                }
+
+                return t(paragraph, translationParams);
+
+            }).join(' ')
+            : ''
+        );
+    }
+
     /**
      * Replaces matching blocks from inputString.
      *
@@ -87,25 +118,18 @@ export class ComponentUtils {
         t: TranslationFunction,
         translationParams: {readonly [key: string]: any} = {}
     ): ReactNode[] {
-
         const startTag: string = '[[';
         const endTag: string = ']]';
-
         let input: string = inputString;
-
         if ( input.length === 0 ) return [];
-
         const blockKeywords: string[] = map(keys(blocks), (k) => `${startTag}${k}${endTag}`);
         if ( blockKeywords.length === 0 ) {
             return [ input ];
         }
-
         const itemsFound: ReactNode[] = [];
-
         const createIndexPairFunc = (str: string) => {
             return (k: string): [ string, number ] => [ k, str.indexOf(k) ];
         };
-
         do {
 
             const indexPairs = map(blockKeywords, createIndexPairFunc(input));
@@ -128,13 +152,67 @@ export class ComponentUtils {
             input = input.substring(foundKeyword.length);
 
         } while ( true );
-
         if ( input.length ) {
             itemsFound.push(<>{t(input, translationParams)}</>);
         }
-
         return itemsFound;
+    }
 
+    /**
+     * Replaces matching blocks from inputString.
+     *
+     * @param t
+     * @param inputString
+     * @param blocks
+     * @param translationParams
+     */
+    public static prepareNodeStringsFromString (
+        inputString: string,
+        blocks: ComponentNodeMap,
+        t: TranslationFunction,
+        translationParams: {readonly [key: string]: any} = {}
+    ): string[] {
+        const startTag: string = '[[';
+        const endTag: string = ']]';
+        let input: string = inputString;
+        if ( input.length === 0 ) return [];
+        const blockKeywords: string[] = map(keys(blocks), (k) => `${startTag}${k}${endTag}`);
+        if ( blockKeywords.length === 0 ) {
+            return [ input ];
+        }
+        const itemsFound: string[] = [];
+        const createIndexPairFunc = (str: string) => {
+            return (k: string): [ string, number ] => [ k, str.indexOf(k) ];
+        };
+        do {
+
+            const indexPairs = map(blockKeywords, createIndexPairFunc(input));
+            const indexes = map(indexPairs, (item: [ string, number ]): number => item[1]);
+            const index = ComponentUtils._findSmallestIndex(indexes);
+            if ( index < 0 ) break;
+
+            const foundPair: [ string, number ] | undefined = find(indexPairs, (item: [ string, number ]): boolean => item[1] === index);
+            const foundKeyword: string | undefined = foundPair ? foundPair[0] : undefined;
+            if ( foundKeyword === undefined ) throw new TypeError(`Could not find matching key for index ${index}`);
+            const foundBlockKey: string = startsWith(foundKeyword, startTag) && endsWith(foundKeyword, endTag) ? foundKeyword.substring(startTag.length, foundKeyword.length - endTag.length) : '';
+            if ( !has(blocks, foundBlockKey) ) throw new TypeError(`Could not find matching block for index ${index} and key ${foundBlockKey} from tag ${foundKeyword}`);
+
+            if ( index !== 0 ) {
+                itemsFound.push(t(input.substring(0, index), translationParams));
+                input = input.substring(index);
+            }
+
+            const b = blocks[foundBlockKey];
+            if (b?.toString) {
+                itemsFound.push(b.toString());
+            }
+            input = input.substring(foundKeyword.length);
+
+        } while ( true );
+        if ( input.length ) {
+            itemsFound.push(t(input, translationParams));
+        }
+        return itemsFound;
     }
 
     private static _findSmallestIndex (
