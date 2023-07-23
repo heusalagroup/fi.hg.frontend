fbdb932d556bfd446f5dd052116b818d0e57fb5b
Jaakko Heusala <jhh@hg.fi>
2023-07-23 19:03:55 +0300

    Renamed RequestClient implementation as RequestClientImpl and RequestClientInterface as RequestClientAdapter

--- HgFrontend.ts
+++ HgFrontend.ts
@@ -1,29 +1,29 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-import { RequestClientInterface } from "../core/requestClient/RequestClientInterface";
+import { RequestClientAdapter } from "../core/requestClient/RequestClientAdapter";
 import { WindowObjectService } from "../core/WindowObjectService";
 import { FetchRequestClient } from "../core/requestClient/fetch/FetchRequestClient";
-import { RequestClient } from "../core/RequestClient";
+import { RequestClientImpl } from "../core/RequestClientImpl";
 
 export class HgFrontend {
 
     /**
      * This method will initialize our libraries using frontend implementations.
      *
-     * Right now it will call `RequestClient.setClient()` with a standard fetch
+     * Right now it will call `RequestClientImpl.setClient()` with a standard fetch
      * implementation.
      *
-     * @param requestClient
+     * @param requestClient The request client adapter to be used by default
      */
     public static initialize (
-        requestClient ?: RequestClientInterface | undefined
+        requestClient ?: RequestClientAdapter | undefined
     ) {
         if (!requestClient) {
             const w = WindowObjectService.getWindow();
             if (!w) throw new TypeError(`HgFrontend.initialize(): Window object could not be found`);
             requestClient = new FetchRequestClient( w.fetch.bind(w) );
         }
-        RequestClient.setClient(requestClient);
+        RequestClientImpl.setClient(requestClient);
     }
 
 }
