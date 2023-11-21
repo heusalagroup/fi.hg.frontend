d756dfa1955674460bab0b1e2c71b857c22a9b8b
Jaakko Heusala <jhh@hg.fi>
2023-11-21 23:25:57 +0200

    Changed VoidCallback as EventCallbackWithArgs<T> so it supports observer's extra information

--- hooks/useServiceEvent.ts
+++ hooks/useServiceEvent.ts
@@ -2,7 +2,7 @@
 
 import { useCallback, useEffect } from "react";
 import { LogService } from "../../core/LogService";
-import { VoidCallback } from "../../core/interfaces/callbacks";
+import { EventCallbackWithArgs, VoidCallback } from "../../core/interfaces/callbacks";
 import { ObserverCallback, ObserverDestructor } from "../../core/Observer";
 import { isPromise } from "../../core/types/Promise";
 
@@ -22,13 +22,13 @@ export function useServiceEvent<T extends keyof any> (
         ): ObserverDestructor
     },
     event: T,
-    callback: VoidCallback
+    callback: EventCallbackWithArgs<T>
 ) {
     const onEventCallback = useCallback(
-        () => {
+        (eventName: T, ...params: any[]) => {
             LOG.debug(`Event "${event.toString()}": Calling callback`);
             try {
-                const p : unknown = callback();
+                const p : unknown = callback(eventName, ...params);
                 if ( isPromise(p) ) {
                     p.catch((err: any) => {
                         LOG.error(`Event "${event.toString()}": Callback error: `, err);
