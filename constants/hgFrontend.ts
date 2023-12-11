08e033469798d7eb77b0af710a5da24e35d45a43
Jaakko Heusala <jhh@hg.fi>
2023-12-12 00:05:26 +0200

    Rebranding for hyperify.io

--- constants/hgFrontend.ts
+++
@@ -1,2 +0,0 @@
-
-export const SCROLL_TO_HERE_DELAY = 10;
--- hooks/useScrollingElement.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
-
-import { useDocument } from "./useDocument";
-
-export function useScrollingElement () : Element | undefined {
-    const document = useDocument();
-    const element = document?.scrollingElement;
-    return element ? element : undefined;
-}
